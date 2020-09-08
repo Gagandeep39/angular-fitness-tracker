789ae08e130eb893a798a129ed1e8d800a05f94c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-08 18:53:13 +0530

    Implemented Login Screen UI

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,10 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submitForm(form: NgForm) {
+    console.log(form.value);
   }
 
 }
