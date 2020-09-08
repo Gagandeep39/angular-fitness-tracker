f466c4b7d7162ea489455d09708a43dd3a109c82
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-08 15:05:55 +0530

    Created a mdoule to store material ocmponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDemoModule } from './material-demo.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialDemoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-demo.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+@NgModule({
+  imports: [MatButtonModule],
+  exports: [MatButtonModule],
+})
+export class MaterialDemoModule {}
