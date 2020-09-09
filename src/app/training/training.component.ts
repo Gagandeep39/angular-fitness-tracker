80e6e76a4b1d6188027abfbdeaf5410e01dfb77d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-09 19:20:40 +0530

    Training component now use training service

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrainingService } from '../services/training.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
-  styleUrls: ['./training.component.css']
+  styleUrls: ['./training.component.css'],
 })
 export class TrainingComponent implements OnInit {
-
   ongoingTraining = false;
+  exerciseSubscription: Subscription;
 
-  constructor() { }
+  constructor(private trainingService: TrainingService) {}
 
   ngOnInit(): void {
+    this.exerciseSubscription = this.trainingService.exerciseChange.subscribe(
+      (newExercise) =>
+        newExercise
+          ? (this.ongoingTraining = true)
+          : (this.ongoingTraining = false)
+    );
   }
 
+  ngOnDestroy(): void {
+    this.exerciseSubscription.unsubscribe();
+  }
 }
