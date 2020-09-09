80841d144d64af9fa9b3dae707ae25ae2db37930
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-09 13:30:29 +0530

    Created a simple card

--- src/app/material-demo.module.ts
+++ src/app/material-demo.module.ts
@@ -10,6 +10,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -24,6 +25,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
@@ -37,6 +39,7 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatToolbarModule,
     MatListModule,
     MatTabsModule,
+    MatCardModule,
   ],
 })
 export class MaterialDemoModule {}
