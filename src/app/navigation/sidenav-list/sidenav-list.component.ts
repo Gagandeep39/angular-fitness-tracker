be6a699de449561c8ca4d868308dedb5fd27f09b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-09 18:28:09 +0530

    Sidenav now shows option based on auth status

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -8,12 +10,22 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class SidenavListComponent implements OnInit {
   @Output()
   toggleSideNav = new EventEmitter<void>();
+  isAuth: boolean;
+  authSubscription: Subscription;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => (this.isAuth = authStatus)
+    );
+  }
 
   onClose() {
     this.toggleSideNav.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
   }
 }
