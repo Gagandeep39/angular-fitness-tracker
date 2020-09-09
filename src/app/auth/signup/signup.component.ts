6091b14ac75b1f020e9d74d39b13ea8421ca1e01
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-09 18:17:06 +0530

    Connected signup to auth service

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,7 +10,7 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent implements OnInit {
   maxDate: Date;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.maxDate = new Date();
@@ -17,6 +18,9 @@ export class SignupComponent implements OnInit {
   }
 
   submitForm(form: NgForm) {
-    console.log(form.value);
+    this.authService.registerUser({
+      email: form.value.email,
+      password: form.value.password,
+    });
   }
 }
