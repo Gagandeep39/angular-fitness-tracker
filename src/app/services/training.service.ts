cf04e266c88bd1dbc61396bec02df4447ab88cb4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-09 19:18:52 +0530

    Added more functions in training service

--- src/app/services/training.service.ts
+++ src/app/services/training.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Exercise } from '../models/exercise';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TrainingService {
-  availableExercise: Exercise[] = [
+  private activeExercise: Exercise;
+  public exerciseChange = new Subject<Exercise>();
+  private availableExercise: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
     { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
@@ -17,5 +20,12 @@ export class TrainingService {
 
   getAllExercises() {
     return [...this.availableExercise];
+  }
+
+  startExercise(selectedId: string) {
+    this.activeExercise = this.availableExercise.find(
+      (exer) => exer.id === selectedId
+    );
+    this.exerciseChange.next({...this.activeExercise});
   }
 }
