2dbb12769c39c55cd6179e53096ebfb6653eec68
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-11 01:04:33 +0530

    Removed ui service loading logic

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
-import { UiService } from 'src/app/services/ui.service';
 import { Subscription, Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
@@ -14,21 +13,16 @@ import { State } from '../../app.reducer';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  isLoading: boolean = false;
   isLoading$: Observable<boolean>;
   loadingSubscription: Subscription;
 
   constructor(
     private authService: AuthService,
-    private uiService: UiService,
     private store: Store<{ ui: State }>
   ) {}
 
   ngOnInit(): void {
     this.isLoading$ = this.store.pipe(map((state) => state.ui.isLoading));
-    this.loadingSubscription = this.uiService.loadStateChanged.subscribe(
-      (loadState) => (this.isLoading = loadState)
-    );
     this.loginForm = new FormGroup({
       email: new FormControl('', {
         validators: [Validators.required, Validators.email],
@@ -44,9 +38,5 @@ export class LoginComponent implements OnInit {
       email: this.loginForm.value.email,
       password: this.loginForm.value.password,
     });
-  }
-
-  ngOnDestroy(): void {
-    this.loadingSubscription.unsubscribe();
   }
 }
