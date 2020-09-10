3d35f5a233326f6cfc0c2201f56a82b68bb34f2a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-10 11:58:21 +0530

    Added paginator

--- src/app/material-demo.module.ts
+++ src/app/material-demo.module.ts
@@ -16,6 +16,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   imports: [
@@ -35,7 +36,8 @@ import { MatSortModule } from '@angular/material/sort';
     MatProgressSpinnerModule,
     MatDialogModule,
     MatTableModule,
-    MatSortModule,  // Required for Sorting table
+    MatSortModule, // Required for Sorting table
+    MatPaginatorModule,
   ],
   exports: [
     MatButtonModule,
@@ -54,7 +56,8 @@ import { MatSortModule } from '@angular/material/sort';
     MatProgressSpinnerModule,
     MatDialogModule,
     MatTableModule,
-    MatSortModule,  // Required for Sorting table
+    MatSortModule, // Required for Sorting table
+    MatPaginatorModule,
   ],
 })
 export class MaterialDemoModule {}
--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -3,6 +3,7 @@ import { TrainingService } from 'src/app/services/training.service';
 import { Exercise } from 'src/app/models/exercise';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-past-training',
@@ -12,6 +13,7 @@ import { MatSort } from '@angular/material/sort';
 export class PastTrainingComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<Exercise>();
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   // Specify columns tht should be rendered, must match names of matColumnDef
   // ID is not beign displayed as its not present here
   displayedColumns: string[] = [
@@ -30,6 +32,7 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   doFilter(filterValue: string) {
