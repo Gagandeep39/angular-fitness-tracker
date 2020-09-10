92fc0e2e1c3e82014ece307783a639b4e0a12103
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-10 15:23:07 +0530

    Added firebase based signin

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,45 +3,50 @@ import { User } from '../models/user';
 import { AuthData } from '../models/auth-data';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   public authChange = new Subject<boolean>();
-  private user: User;
+  private isAuthenticated: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private angularFireAuth: AngularFireAuth
+  ) {}
 
   registerUser(authData: AuthData) {
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 10000).toString(),
-    };
-    this.authChange.next(true);
-    this.router.navigate(['/training']);
+    this.angularFireAuth
+      .createUserWithEmailAndPassword(authData.email, authData.password)
+      .then((result) => {
+        this.authChange.next(true);
+        this.isAuthenticated = true;
+        this.router.navigate(['/training']);
+      })
+      .catch((error) => console.log(error));
   }
 
   login(authData: AuthData) {
-    this.user = {
-      email: authData.email,
-      userId: Math.round(Math.random() * 10000).toString(),
-    };
-    this.authChange.next(true);
-    this.router.navigate(['/training']);
+    this.angularFireAuth
+      .signInWithEmailAndPassword(authData.email, authData.password)
+      .then((result) => {
+        console.log(result);
+        this.isAuthenticated = true;
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
+      })
+      .catch((error) => console.log(error));
   }
 
   logout() {
-    this.user = null;
+    this.isAuthenticated = true;
     this.authChange.next(false);
     this.router.navigate(['/login']);
   }
 
-  getUser() {
-    return { ...this.user };
-  }
-
   isAuth() {
-    return this.user != null;
+    return this.isAuthenticated;
   }
 }
