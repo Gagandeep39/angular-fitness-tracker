35836c817258849f6a622fb1f53733d2ed0e54ec
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-09-10 13:32:05 +0530

    Data now fetched from server and displayed async

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { TrainingService } from 'src/app/services/training.service';
 import { Exercise } from 'src/app/models/exercise';
 import { NgForm } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -9,15 +12,31 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
-  trainings: Exercise[] = [];
+  trainings: Observable<any>;
 
-  constructor(private trainingService: TrainingService) {}
+  constructor(
+    private trainingService: TrainingService,
+    private firestoreDb: AngularFirestore
+  ) {}
 
   ngOnInit(): void {
-    this.trainings = this.trainingService.getAllExercises();
+    this.trainings = this.firestoreDb
+      .collection('availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map((docArray) => {
+          return <Exercise[]>docArray.map((doc) => {
+            const otherData: Object = doc.payload.doc.data();
+            return {
+              id: doc.payload.doc.id,
+              ...otherData,
+            };
+          });
+        })
+      );
   }
 
   onStartTraining(form: NgForm) {
-    this.trainingService.startExercise(form.value.exercise)
+    this.trainingService.startExercise(form.value.exercise);
   }
 }
